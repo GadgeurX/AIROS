0c306a865e03bf3bf0ee1fd095ebc59187922f74
Romain <romainallee@gmail.com>
2023-10-17 15:05:45 +0200

    memory

--- build/z80/ti/CMakeFiles/AIROS.8xu.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/AIROS.8xu"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/AIROS.8xu.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/z80/ti/CMakeFiles/crt0.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/crt0"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/crt0.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
